1d82feb5ebdee2591fe4985c6d121eeb3256f406
Zhe He <hezhe88@gmail.com>
2016-07-07 10:02:49 -0400

    Monto Carlo to get Min-cut leafs in main.py

--- import_tree_from_xml.py
+++ import_tree_from_xml.py
@@ -4,27 +4,38 @@ import xml.etree.ElementTree as ET
 
 from NodeDef import Node
 
-xml_filename = ''
-if len(sys.argv) == 2:
-    xml_filename = sys.argv[1]
-else:
-    xml_filename = os.path.join('example', 'example.xml')
 
-tree = ET.parse(xml_filename)
-all_nodes = tree.getroot()
-node_dict = {}
+def import_tree_from_xml(xml_filename):
+    tree = ET.parse(xml_filename)
+    all_nodes = tree.getroot()
+    node_dict = {}
 
-for child in all_nodes:
-    child_name = child.attrib['id']
-    logic = child.find('gate').text
-    node_dict[child_name] = Node(child_name, logic)
+    # As we will perform monte carlo on leaves, keep a dict for easier access.
+    leaf_node_dict = {}
 
-for child in all_nodes:
-    child_name = child.attrib['id']
-    for dep in child.findall('dep'):
-        dep_name = dep.text
-        if dep_name in node_dict:
-            node_dict[child_name].add_child(node_dict[dep_name])
-        else:  # Special cases for nodes in <dep> but not <node>
-            node_dict[dep_name] = Node(dep_name)
-            node_dict[child_name].add_child(node_dict[dep_name])
+    # First iter, build all nodes (except nodes only appear in <dep>)
+    # and put them in node_dict
+    for node in all_nodes:
+        node_name = node.attrib['id']
+        logic = node.find('gate').text
+        if node_name in node_dict:
+            raise ValueError("Different nodes with same name")
+        node_dict[node_name] = Node(node_name, logic)
+
+    # Second iter, build parent-child relationship and put leaves in another
+    # dict.
+    for node in all_nodes:
+        node_name = node.attrib['id']
+        if node.findall('dep'):
+            for dep in node.findall('dep'):
+                dep_name = dep.text
+                if dep_name in node_dict:
+                    node_dict[node_name].add_child(node_dict[dep_name])
+                else:  # Special cases for nodes in <dep> but not <node>
+                    node_dict[dep_name] = Node(dep_name)
+                    node_dict[node_name].add_child(node_dict[dep_name])
+                    leaf_node_dict[dep_name] = node_dict[dep_name]
+        else:
+            leaf_node_dict[node_name] = node_dict[node_name]
+
+    return node_dict, leaf_node_dict
